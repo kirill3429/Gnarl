8695cf9813decb5feb2a8370c3cafcef363d3595
kirill3429 <kirill3429@yandex.ru>
2022-11-30 02:50:25 +0300

    laser gun almost done

---
+++ Scripts/Buildings/Guns/Laser.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] private float rayDistance;
+    [SerializeField] private float laserDamage;
+    [SerializeField] private float laserDuration;
+
+    private float startTime;
+
+    private LineRenderer lineRenderer;
+    private Transform myTransform;
+    private GameObject currentTarget;
+    private GameObject lastTarget;
+    private Health targetHealth;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        myTransform = transform;
+    }
+    private void Awake()
+    {
+        startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time - startTime > laserDuration)
+        {
+            TickRay();
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+
+    private void TickRay()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(myTransform.position, myTransform.up, rayDistance);
+
+        if (hit == false)
+        {
+            UpdateBeam(myTransform.up * rayDistance);
+        }
+        else
+        {
+            currentTarget = hit.collider.gameObject;
+            if (currentTarget != lastTarget)
+            {
+                targetHealth = currentTarget.GetComponent<Health>();
+                targetHealth.TakeDamage(laserDamage);
+            }
+            else
+            {
+                targetHealth.TakeDamage(laserDamage);
+            }
+            UpdateBeam(hit.point);
+        }
+        
+    }
+
+    private void UpdateBeam(Vector2 end)
+    {
+        lineRenderer.SetPosition(0, myTransform.position);
+        lineRenderer.SetPosition(1, end);
+    }
+}
---
+++ Scripts/Buildings/Guns/LaserGun.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class LaserGun : Build
+{
+    [SerializeField] private Laser laser;
+
+
+    public override void Activate()
+    {
+        isActive = true;
+    }
+
+    public override void Deactivate()
+    {
+        isActive = false;
+    }
+
+    public override void Perform()
+    {
+        base.Perform();
+        laser.gameObject.SetActive(true);
+    }
+
+}
